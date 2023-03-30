39640bb2b8521490c3d7ea684496fd707085c499
Hao1122 <chinleehao@gmail.com>
2023-03-30 15:11:31 +0800

    Authentication

---
+++ controllers/authController.js
@@ -0,0 +1,62 @@
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const handleLogin = async (req, res) => {
+  const cookies = req.cookies;
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: "Username and password are required." });
+  const foundUser = await User.findOne({ username: user }).exec();
+  if (!foundUser) return res.sendStatus(401); //Unauthorized
+  // evaluate password
+  const match = await bcrypt.compare(pwd, foundUser.password);
+  if (match) {
+    const roles = Object.values(foundUser.roles);
+    // create JWTs
+    const accessToken = jwt.sign(
+      {
+        UserInfo: {
+          username: foundUser.username,
+          roles: roles,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "500s" }
+    );
+    const newRefreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    const newRefreshTookenArray = !cookies?.jwt
+      ? foundUser.refreshToken
+      : foundUser.refreshToken.filter((rt) => rt !== cookies.jwt);
+
+    if (cookies?.jwt)
+      res.clearCookie("jwt", {
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
+      });
+
+    // Saving refreshToken with current user
+    foundUser.refreshToken = [...newRefreshTookenArray, newRefreshToken];
+    const result = await foundUser.save();
+    console.log(result);
+
+    res.cookie("jwt", newRefreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      maxAge: 24 * 60 * 60 * 1000,
+    }); //secure: true,
+    res.json({ accessToken });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { handleLogin };
